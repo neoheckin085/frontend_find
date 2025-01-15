acdf67d805292c657c6ee7fd370428984856e1c5
Qadafi21 <qadafiyusuf18@gmail.com>
2025-01-15 13:32:03 +0800

    Splash Screen

---
+++ src/Find.jsx
@@ -0,0 +1,22 @@
+import { View, Text, ImageBackground, Image } from 'react-native';
+import React from 'react';
+
+const Find = () => {
+    return (
+        <ImageBackground 
+            source={require('./assets/Background.png')} // Path ke gambar background
+            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} // Pastikan background menutupi seluruh layar
+        >
+            <View style={{ alignItems: 'center', marginBottom: 5 }}>
+                <Image 
+                    style={{ height: 450, width: 450 }} 
+                    source={require('./assets/Find.png')} 
+                />
+            </View>
+            <Text style={{ marginTop: 10, fontSize: 70, color: 'white' }}>F!ND</Text>
+        </ImageBackground>
+    );
+};
+
+export default Find;
+
