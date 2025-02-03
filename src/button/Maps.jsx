af6af6fb48baab2e169d99229e3193ba1de7443c
Qadafi21 <qadafiyusuf18@gmail.com>
2025-02-03 12:10:20 +0800

    Tesmaps

--- src/button/Maps.jsx
+++ src/button/Maps.jsx
@@ -1,12 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 const Maps = () => {
   return (
-    <View>
-      <Text>Maps</Text>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
+          title="Marker Title"
+          description="Marker Description"
+        />
+      </MapView>
     </View>
-  )
-}
+  );
+};
 
-export default Maps
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
+export default Maps;
